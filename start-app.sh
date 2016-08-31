34a6daa8f059728684fd0d9ac1a0f1b508f3c34d
zppro <zpprozppro@gmail.com>
2016-08-31 08:58:13 +0800

    0.8.6支持使用nvm切换node版本

--- start-app.sh
+++ start-app.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 cd ~/github/fsrok/src/
+source $NVM_DIR/nvm.sh
+nvm use 0.12.2
 node --harmony app.js isProduction=true client.bulidtarget=anyang-1.0 db.mongodb.user=fs db.mongodb.password=fs2015 db.mongodb.server=localhost db.mongodb.port=27017 db.mongodb.database=fs secure.authSecret=anyang port=80
